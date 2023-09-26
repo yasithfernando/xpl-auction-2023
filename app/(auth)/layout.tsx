1524a4b4fc58645ea8eca467e3fc3644c91a6686
yasithfernando <iyfernando42@gmail.com>
2023-09-26 19:49:40 +0530

    add favicon

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'XPL Auction',
+  title: 'XPL 2023',
   description: 'An auction platform for XPL 2023',
 }
 
