4a798cdd1ed784a6881041dcd0fcf0c624b76563
ismailnyza <ismailnyzarsl@gmail.com>
2023-09-26 13:58:58 +0530

    🌈 style(system): file structure
    changed file structure of project to accomadate a more modular structure

---
+++ app/register/page.tsx
@@ -0,0 +1,12 @@
+
+export default function page() {
+    return (
+        <>
+        <div className="formContainer">
+            <form>
+                
+            </form>
+        </div>
+        </>
+    )
+}
